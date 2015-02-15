00a48db44ed5776fab23f5ba50e1d9a5ae8b7d7c
Vadim <pv@petrovvadim.com>
2015-02-15 23:21:41 +0300

    Добавил обработчик для вотермарка

--- app/blocks/block-upload/_block-upload.js
+++ app/blocks/block-upload/_block-upload.js
@@ -1,9 +1,17 @@
 // Блок загрузки картинок block-upload
-;(function ($){
-	$('.block-upload__input').fileupload({
+(function ($){
+	// Исходное изображение
+	$('.block-upload__input-main').fileupload({
 		dataType: 'json',
 		done: function (e, data) {
-			//$('.main-img-upload-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
+			$('.block-upload__input-main-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
+		}
+	});
+	// Водяной знак
+	$('.block-upload__input-watermark').fileupload({
+		dataType: 'json',
+		done: function (e, data) {
+			$('.block-upload__input-watermark-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
 		}
 	});
 })(jQuery);
