1009ca2eb8b505bea5dbc0ce1f92edd8cc86dfa6
steelcat <me@steelcat.org>
2015-02-15 09:16:14 +0300

    Предотвращение множественного выбора позиции

--- app/blocks/block-place/_block-place.js
+++ app/blocks/block-place/_block-place.js
@@ -1,6 +1,10 @@
 // Блок положения картинок block-place
 $(function() {
     $( ".block-place__control__buttonset" ).selectable({
-        selecting: function( event, ui ) {}
+        stop: function(e, ui) {
+            $(".ui-selected:first", this).each(function() {
+                $(this).siblings().removeClass("ui-selected");
+            });
+        }
     });
 });
