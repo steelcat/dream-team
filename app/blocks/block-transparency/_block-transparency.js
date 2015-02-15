8494024e254655ab744de89a513a009953694f28
Steelcat <steelpuppy@gmail.com>
2015-02-15 17:05:49 +0300

    Подправил слайдер, но работает коряво

--- app/blocks/block-transparency/_block-transparency.js
+++ app/blocks/block-transparency/_block-transparency.js
@@ -12,26 +12,26 @@
     makeView : function(){
     	
       //настраиваем слайдер
-		  $("#slider").slider({
+		  $(".block-transparency__slider").slider({
         max: 1, //максимальное значение
         min: 0.01, //минимальное значение
         step: 0.01, //шаг изменения значения
         
         // изменяет значение opacity при движении ползунка
         slide: function(event, ui) {
-           var value = $("#slider").slider( "option", "value" );//получаем текущее значение
+           var value = $(".block-transparency__slider").slider( "option", "value" );//получаем текущее значение
            $('.block-result__watermark').css({opacity: value}); //устанавливаем свойство элемента
         },
 
         // изменяет значение opacity при отпускании ползунка
         change: function(event, ui) {          
-          var value = $("#slider").slider( "option", "value" );//получаем текущее значение
+          var value = $(".block-transparency__slider").slider( "option", "value" );//получаем текущее значение
           $('.block-result__watermark').css({opacity: value});//устанавливаем свойство элемента
         }
       });
 
       //устанавливаем свойство элемента 0,9 после загрузки старницы
-      $("#slider").slider( "option", "value", 0.9 );
+      $(".block-transparency__slider").slider( "option", "value", 0.9 );
     }
 
   }
