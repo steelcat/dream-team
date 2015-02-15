4424ceaa0473b0fb05f6f38577c1e011942bfa49
MaxKashintsev <m.kashintcev@gmail.com>
2015-02-15 11:16:15 +0300

    Добавил функционал слайдера

--- app/blocks/block-transparency/_block-transparency.js
+++ app/blocks/block-transparency/_block-transparency.js
@@ -1,1 +1,41 @@
-//test
+// block-transparency
+
+(function() {    
+
+  var app = {
+
+    // Инициализация
+    initialize : function () {
+    	app.makeView();    	  	
+    },
+
+    makeView : function(){
+    	
+      //настраиваем слайдер
+		  $("#slider").slider({
+        max: 1, //максимальное значение
+        min: 0.01, //минимальное значение
+        step: 0.01, //шаг изменения значения
+        
+        // изменяет значение opacity при движении ползунка
+        slide: function(event, ui) {
+           var value = $("#slider").slider( "option", "value" );//получаем текущее значение
+           $('.block-result__watermark').css({opacity: value}); //устанавливаем свойство элемента
+        },
+
+        // изменяет значение opacity при отпускании ползунка
+        change: function(event, ui) {          
+          var value = $("#slider").slider( "option", "value" );//получаем текущее значение
+          $('.block-result__watermark').css({opacity: value});//устанавливаем свойство элемента
+        }
+      });
+
+      //устанавливаем свойство элемента 0,9 после загрузки старницы
+      $("#slider").slider( "option", "value", 0.9 );
+    }
+
+  }
+
+  app.initialize();
+
+}());
