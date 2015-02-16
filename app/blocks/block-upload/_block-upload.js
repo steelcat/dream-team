c13031b9a5716b2e40bce43083d3a55b988c6dcf
Vadim <pv@petrovvadim.com>
2015-02-16 13:01:55 +0300

    Добавил замену background-image блокам

--- app/blocks/block-upload/_block-upload.js
+++ app/blocks/block-upload/_block-upload.js
@@ -1,17 +1,36 @@
 // Блок загрузки картинок block-upload
 (function ($){
-	// Исходное изображение
-	$('.block-upload__input-main').fileupload({
-		dataType: 'json',
-		done: function (e, data) {
-			$('.block-upload__input-main-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
-		}
-	});
-	// Водяной знак
-	$('.block-upload__input-watermark').fileupload({
-		dataType: 'json',
-		done: function (e, data) {
-			$('.block-upload__input-watermark-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
-		}
-	});
+	var app = {
+		initialize: function () {
+			this.setListners();
+		},
+		setListners: function () {
+			$('.block-upload__input-main, .block-upload__input-watermark').on('click', app.fileUploadFunc);
+		},
+		fileUploadFunc: function () {
+			var input = $(this);
+			if(input.hasClass('block-upload__input-main')){
+				// Значит это
+				// Исходное изображение
+				input.fileupload({
+					dataType: 'json',
+					done: function (e, data) {
+						$('.block-upload__input-main-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
+						$('.block-result__original').css( 'background-image', 'url(files/' + (data.files[0].name) + ')' );
+					}
+				});
+			} else {
+				// Иначе это
+				// Водяной знак
+				input.fileupload({
+					dataType: 'json',
+					done: function (e, data) {
+						$('.block-upload__input-watermark-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
+						$('.block-result__watermark').css( 'background-image', 'url(files/' + (data.files[0].name) + ')' );
+					}
+				});
+			};
+		},
+	};
+	app.initialize();
 })(jQuery);
