a735f3fd89aac1e96c79ed37c0a497dcc25c6c3c
Steelcat <steelpuppy@gmail.com>
2015-02-17 23:09:33 +0300

    Сделал ресайз и подстановку изображения водяного знака штатными средствами

--- app/blocks/block-upload/_block-upload.js
+++ app/blocks/block-upload/_block-upload.js
@@ -16,7 +16,7 @@
 					dataType: 'json',
 					done: function (e, data) {
 						$('.block-upload__input-main-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
-						$('.block-result__original').css( 'background-image', 'url(upload/resize/' + (data.files[0].name) + ')' );
+						$('.block-result__original').css( 'background-image', 'url(upload/' + (data.files[0].name) + ')' );
 					}
 				});
 			} else {
@@ -26,7 +26,7 @@
 					dataType: 'json',
 					done: function (e, data) {
 						$('.block-upload__input-watermark-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
-						$('.block-result__watermark').css( 'background-image', 'url(upload/resize/' + (data.files[0].name) + ')' );
+						$('.block-result__watermark').html('<img src="upload/' + data.files[0].name + '">');
 					}
 				});
 			};
--- app/php/UploadHandler/UploadHandler.php
+++ app/php/UploadHandler/UploadHandler.php
@@ -131,6 +131,8 @@ class UploadHandler
                 // Сделал кроп, так как кратинка должна точно вписываться в размеры контейнера 651 на 534 пикселя
 
                 'resize' => array(
+                    'upload_dir' => dirname($this->get_server_var('SCRIPT_FILENAME')).'/../upload/',
+                    'upload_url' => $this->get_full_url().'/../upload/',
                     'crop' => true,
                     'max_width' => 651,
                     'max_height' => 534
