92d4d2d46b6a7b7fc72c599359001bdaf292e3cb
Steelcat <steelpuppy@gmail.com>
2015-02-18 11:19:50 +0300

    Добавил обработку ошибок

--- app/blocks/block-upload/_block-upload.js
+++ app/blocks/block-upload/_block-upload.js
@@ -3,9 +3,21 @@
 	var app = {
 		initialize: function () {
 			this.setListners();
+			this.ModalSizeError();
 		},
 		setListners: function () {
 			$('.block-upload__input-main, .block-upload__input-watermark').on('click', app.fileUploadFunc);
+		},
+		ModalSizeError: function() {
+			$('.block-upload-size-error').dialog({
+				autoOpen: false,
+				modal: true,
+				buttons: {
+					Ok: function() {
+						$( this ).dialog( "закрыть" );
+					}
+				}
+			});
 		},
 		fileUploadFunc: function () {
 			var input = $(this);
@@ -16,7 +28,9 @@
 					dataType: 'json',
 					done: function (e, data) {
 
-						console.log(data.files[0]);
+						if (data.result.files[0].error) {
+							$('.block-upload-size-error').dialog( 'open' );
+						}
 						$('.block-upload__input-main-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
 						$('.block-result__original').css( 'background-image', 'url(upload/' + (data.files[0].name) + ')' );
 					}
--- app/php/UploadHandler/UploadHandler.php
+++ app/php/UploadHandler/UploadHandler.php
@@ -84,7 +84,7 @@ class UploadHandler
             'accept_file_types' => '/.+$/i',
             // The php.ini settings upload_max_filesize and post_max_size
             // take precedence over the following max_file_size setting:
-            'max_file_size' => 8 * 1024 * 1024,
+            'max_file_size' => 3 * 1024 * 1024,
             'min_file_size' => 1,
             // The maximum number of files for the upload directory:
             'max_number_of_files' => null,
