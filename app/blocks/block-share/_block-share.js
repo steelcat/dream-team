f364fe501735c14643e6963804b5f0cafd2a2203
Vadim <pv@petrovvadim.com>
2015-02-18 14:11:19 +0300

    Добавил title, description. Написал js кнопкам шаринга

---
+++ app/blocks/block-share/_block-share.js
@@ -0,0 +1,33 @@
+(function ($){
+	var app = {
+		initialize: function () {
+			app.setListners();
+		},
+		setListners: function () {
+			$('.social-btns__toggler').on('click', app.toggleShareBox);
+			$('.block-share__social-btns__btn__link').on('click', app.sharePage);
+		},
+		toggleShareBox: function () {
+			$('.block-share__social-btns').stop(true, true).animate({width: 'toggle'}); // Показывает-скрывает кнопки
+		},
+		sharePage: function (e) {
+			var link = $(this),
+				win = window,
+				title = $(document).prop('title'),
+				desc = $('meta[name="description"]').attr('content'),
+				url = win.location.href;
+
+			if(link.hasClass('fb')){
+				win.open( 'https://www.facebook.com/sharer/sharer.php?url=' + url, 'facebook', 'width=640, height=200');
+			}
+			if(link.hasClass('vk')){
+				win.open( 'https://vk.com/share.php?url=' + url + '&title=' + title + '&description=' + desc, 'vkontakte', 'width=640, height=480' );
+			}
+			if(link.hasClass('tw')){
+				win.open( 'https://twitter.com/intent/tweet?status=' + title + ' - ' + url, 'twitter', 'width=640, height=260' );
+			};
+			e.preventDefault();
+		}
+	};
+	app.initialize();
+})(jQuery);
