f0fe162ea0739f5c5fc19637cd2be877ac85472a
Steelcat <steelpuppy@gmail.com>
2015-02-21 22:40:53 +0300

    Доделал блок квадрата положения

--- app/blocks/block-place/_block-place.js
+++ app/blocks/block-place/_block-place.js
@@ -18,7 +18,7 @@
 		},
 		listen: function(){
 			console.log('Прослушка place__control__spinners запущена');
-			$( ".block-place__control__buttonset" ).on( "selectablecreate", app.setGridPos);
+			$( ".block-place__control__buttonset" ).on( "selectableselected", app.setGridPos);
 			$(".up-button").on('mousedown', { asx: "up" }, app.setPos);
 			$(".down-button").on('mousedown', { asx: "down" }, app.setPos);
 			$(document).on('mouseup', app.setPosStop);
@@ -36,8 +36,49 @@
 				}
 			})
 		},
-		setGridPos: function (event) {
-			console.log('Выбран квадрат сетки положения');
+		setGridPos: function (event,ui) {
+			console.log('Выбран квадрат сетки положения = '+ ui.selected.id);
+			switch (ui.selected.id) {
+				case 'top-left':
+					console.log('top-left');
+					$(".block-result__watermark-image").css("top", "0px").css("left", "0px");
+					break;
+				case 'top':
+					console.log('top');
+					$(".block-result__watermark-image").css("top", "0px").css("left", app.maxWidth/2 + "px");
+					break;
+				case 'top-right':
+					console.log('top-right');
+					$(".block-result__watermark-image").css("top", "0px").css("left", app.maxWidth + "px");
+					break;
+				case 'center-left':
+					console.log('center-left');
+					$(".block-result__watermark-image").css("top", app.maxHeight/2 + "px").css("left", "0px");
+					break;
+				case 'center':
+					console.log('center');
+					$(".block-result__watermark-image").css("top", app.maxHeight/2 + "px").css("left", app.maxWidth/2 + "px");
+					break;
+				case 'center-right':
+					console.log('center-right');
+					$(".block-result__watermark-image").css("top", app.maxHeight/2 + "px").css("left", app.maxWidth + "px");
+					break;
+				case 'bottom-left':
+					console.log('top-left');
+					$(".block-result__watermark-image").css("top", app.maxHeight + "px").css("left", "0px");
+					break;
+				case 'bottom':
+					console.log('top');
+					$(".block-result__watermark-image").css("top", app.maxHeight + "px").css("left", app.maxWidth/2 + "px");
+					break;
+				case 'bottom-right':
+					console.log('top-right');
+					$(".block-result__watermark-image").css("top", app.maxHeight + "px").css("left", app.maxWidth + "px");
+					break;
+				default:
+					console.log('default');
+					break
+			}
 		},
 		setPos: function(event){
 			var _this = this;
@@ -51,10 +92,10 @@
 
 				if(asix === "y-value") {
 					if(value>app.maxHeight){value = app.maxHeight;}
-					$(".block-result__watermark img").css("top", value + "px");
+					$(".block-result__watermark-image").css("top", value + "px");
 				} else {
 					if(value>app.maxWidth){value = app.maxWidth;}
-					$(".block-result__watermark img").css("left", value + "px");
+					$(".block-result__watermark-image").css("left", value + "px");
 				}
 				patch.attr("value",value);//Присваиваем значение в клетках
 			}, 10);//TODO уменьшить скорость смены позиции.
@@ -63,8 +104,8 @@
 			clearInterval(app.timeout);
 		},
 		getImageParam: function(){
-			app.width = $(".block-result__watermark img").width();
-			app.height = $(".block-result__watermark img").height();
+			app.width = $(".block-result__watermark-image").width();
+			app.height = $(".block-result__watermark-image").height();
 		},
 		getMaxMinPos: function(){
 			app.maxWidth = 651 - app.width;
