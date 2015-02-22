7056d58c3ed5cf70038e57902c819a7530c56dd1
Steelcat <steelpuppy@gmail.com>
2015-02-22 17:47:36 +0300

    Сделал передачу ккординат из окна результата

--- app/blocks/block-result/_block-result.js
+++ app/blocks/block-result/_block-result.js
@@ -2,14 +2,20 @@
 (function ($){
 	var app = {
 		initialize: function () {
-			this.setListners();
 			this.dragWatermark();
+			this.setListners();
 		},
 		setListners: function () {
-
+			$( '.block-result__watermark-image' ).on( "drag", app.setPos );
 		},
 		dragWatermark: function () {
 			$('.block-result__watermark-image').draggable({ containment: ".block-result__watermark", scroll: false });
+		},
+		setPos: function () {
+			var watermarkImage = document.getElementById('watermark-image');
+			var x = watermarkImage.offsetLeft;
+			var y = watermarkImage.offsetTop;
+			blocksLibrary.setValue(x,y);
 		}
 	};
 	app.initialize();
