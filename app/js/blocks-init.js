e817202025cd7259344661bbe1c9903ae6d97241
Steelcat <steelpuppy@gmail.com>
2015-02-23 23:27:32 +0300

    Переделал ползунок прозрачности водяного знака

--- app/js/blocks-init.js
+++ app/js/blocks-init.js
@@ -1,6 +1,7 @@
 // Подключаем методы и прослушку для всех блоков
 (function () {
 	blockResult.dragWatermark(); // Инициализируем перетаскивание водяного знака
+	blockTransparency.makeSlider(); // Инициализируем слайдер прозрачности
 	$('.block-upload__input-original').on('click', blockUpload.fileUploadOriginal); // Слушаем кнопку загрузки оригнала
 	$('.block-upload__input-watermark').on('click', blockUpload.fileUploadWatermark); // Слушаем кнопку загрузки водяного знака
 	$('.block-download__button-submit').on('click', blockDownload.init); // Слушаем кнопку скаичвания итоговой картинки
