8705dedc2396c2333b9b06b6fea273b96f30b7e5
Steelcat <steelpuppy@gmail.com>
2015-02-23 23:20:41 +0300

    Убрал блок сообщения об ошибке

--- app/js/blocks-init.js
+++ app/js/blocks-init.js
@@ -1,10 +1,8 @@
-(function() {
-	// Подключаем прослушку для всех блоков
-	(function () {
-		blockResult.dragWatermark(); // Инициализируем перетаскивание водяного знака
-		$('.block-upload__input-original').on('click', blockUpload.fileUploadOriginal); // Слушаем кнопку загрузки оригнала
-		$('.block-upload__input-watermark').on('click', blockUpload.fileUploadWatermark); // Слушаем кнопку загрузки водяного знака
-		$('.block-download__button-submit').on('click', blockDownload.init); // Слушаем кнопку скаичвания итоговой картинки
-		$( '.block-result__watermark-image' ).on( 'drag', blockResult.setPos ); // Слушаем событие перетаскивания водяного знака
-	})();
+// Подключаем методы и прослушку для всех блоков
+(function () {
+	blockResult.dragWatermark(); // Инициализируем перетаскивание водяного знака
+	$('.block-upload__input-original').on('click', blockUpload.fileUploadOriginal); // Слушаем кнопку загрузки оригнала
+	$('.block-upload__input-watermark').on('click', blockUpload.fileUploadWatermark); // Слушаем кнопку загрузки водяного знака
+	$('.block-download__button-submit').on('click', blockDownload.init); // Слушаем кнопку скаичвания итоговой картинки
+	$( '.block-result__watermark-image' ).on( 'drag', blockResult.setPos ); // Слушаем событие перетаскивания водяного знака
 })();
