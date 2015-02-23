6acd91d413c7027399c34790b7746ad5bd4d212e
Steelcat <steelpuppy@gmail.com>
2015-02-23 23:17:48 +0300

    Переделал блок результатов block-result

--- app/blocks/block-result/_block-result.js
+++ app/blocks/block-result/_block-result.js
@@ -1,23 +1,13 @@
 // Блок показа картинок block-result
 
-(function ($){
-	var app = {
-		initialize: function () {
-			this.dragWatermark();
-			this.setListners();
-		},
-		setListners: function () {
-			$( '.block-result__watermark-image' ).on( "drag", app.setPos );
-		},
-		dragWatermark: function () {
-			$('.block-result__watermark-image').draggable({ containment: ".block-result__watermark", scroll: false });
-		},
-		setPos: function () {
-			var watermarkImage = document.getElementById('watermark-image');
-			var x = watermarkImage.offsetLeft;
-			var y = watermarkImage.offsetTop;
-			blocksLibrary.setValue(x,y);
-		}
-	};
-	app.initialize();
-})(jQuery);
+var blockResult = {
+	dragWatermark: function () {
+		$('.block-result__watermark-image').draggable({ containment: ".block-result__watermark", scroll: false });
+	},
+	setPos: function () {
+		var watermarkImage = document.getElementById('watermark-image');
+		var x = watermarkImage.offsetLeft;
+		var y = watermarkImage.offsetTop;
+		blocksLibrary.setValue(x,y);
+	}
+};
