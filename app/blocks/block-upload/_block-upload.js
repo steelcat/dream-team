91e69bb463cb9347037ccd019c703d639f4b2057
Steelcat <steelpuppy@gmail.com>
2015-02-26 23:08:46 +0300

    Сделал обработку ошибок при загрузке

--- app/blocks/block-upload/_block-upload.js
+++ app/blocks/block-upload/_block-upload.js
@@ -22,15 +22,18 @@ var blockUpload = {
 				var img = new Image();
 				img.src = 'files/' + (data.result.files[0].url);
 				console.log(data.result.files[0]);
-				if (data.result.files[0].error) {
+				if ((data.result.files[0].error)&&(data.result.files[0].error!=='Failed to resize image')) {
 					console.log(data.result.files[0].error);
-				}//TODO При большой длине названия файла оно может вылезти за пределы блока, сделать ограничение длины названия
-				$('.block-upload__input-original-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
-				$('.block-result__original').html('<img id="original-image" class="block-result__original-image" src="files/' + (data.result.files[0].url) + '">');
-				$('.block-result__original').css('background', 'none');
-				img.onload = function() {
-					$('.block-result__original').css('height', img.height);
-					$('.block-result__original').css('width', img.width);
+					$('.block-upload__input-original-imitation').text('Ошибка').css('color', 'red');
+				} else {
+					//TODO При большой длине названия файла оно может вылезти за пределы блока, сделать ограничение длины названия
+					$('.block-upload__input-original-imitation').text(data.files[0].name).css('color', '#9eb2c0'); // добавит название файла в блок имитирующий input
+					$('.block-result__original').html('<img id="original-image" class="block-result__original-image" src="files/' + (data.result.files[0].url) + '">');
+					$('.block-result__original').css('background', 'none');
+					img.onload = function() {
+						$('.block-result__original').css('height', img.height);
+						$('.block-result__original').css('width', img.width);
+					}
 				}
 			}
 		});
@@ -43,16 +46,19 @@ var blockUpload = {
 				var img = new Image();
 				img.src = 'files/' + (data.result.files[0].url);
 				console.log(data.result.files[0]);
-				if (data.result.files[0].error) {
+				if ((data.result.files[0].error)&&(data.result.files[0].error!=='Failed to resize image')) {
 					console.log(data.result.files[0].error);
-				}//TODO При большой длине названия файла оно может вылезти за пределы блока, сделать ограничение длины названия
-				$('.block-upload__input-watermark-imitation').text(data.files[0].name); // добавит название файла в блок имитирующий input
-				$('.block-result__watermark').html('<img id="watermark-image" class="block-result__watermark-image" src="files/' + (data.result.files[0].url) + '">');
-				img.onload = function() {
-					$('.block-result__watermark').css('height', img.height);
-					$('.block-result__watermark').css('width', img.width);
+					$('.block-upload__input-watermark-imitation').text('Ошибка').css('color', 'red');
+				} else {
+					//TODO При большой длине названия файла оно может вылезти за пределы блока, сделать ограничение длины названия
+					$('.block-upload__input-watermark-imitation').text(data.files[0].name).css('color', '#9eb2c0'); // добавит название файла в блок имитирующий input
+					$('.block-result__watermark').html('<img id="watermark-image" class="block-result__watermark-image" src="files/' + (data.result.files[0].url) + '">');
+					img.onload = function () {
+						$('.block-result__watermark').css('height', img.height);
+						$('.block-result__watermark').css('width', img.width);
+					}
+					blockResult.dragWatermark();
 				}
-				blockResult.dragWatermark();
 			}
 		});
 	}
