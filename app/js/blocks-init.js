9f5d4164907534bb4cf4b061063fb282717eb22d
Steelcat <steelpuppy@gmail.com>
2015-02-28 17:24:49 +0300

    Доделал кнопку сброса

--- app/js/blocks-init.js
+++ app/js/blocks-init.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	blockPlace.listen(); //Слушаем события блока
 	$('.block-upload__input-original').on('click', blockUpload.fileUploadOriginal); // Слушаем кнопку загрузки оригнала
 	$('.block-upload__input-watermark').on('click', blockUpload.fileUploadWatermark); // Слушаем кнопку загрузки водяного знака
+	$('.block-download__button-reset').on('click', blocksLibrary.reset); // Слушаем кнопку сброса параметров наложения водяого знака
 	$('.block-download__button-submit').on('click', blockDownload.init); // Слушаем кнопку скаичвания итоговой картинки
 	$( '.block-result__watermark' ).on( 'drag', blockResult.setPos ); // Слушаем событие перетаскивания водяного знака
-})();
+});
--- app/js/blocks-lib.js
+++ app/js/blocks-lib.js
@@ -2,5 +2,12 @@ var blocksLibrary = {
 	setValue: function setValue(x,y) {
 		$('#x-value').attr('value', x);
 		$('#y-value').attr('value', y);
+	},
+	reset: function reset() {
+		$('.block-result__original').css('left', 0);
+		$('.block-result__original').css('top', 0);
+		$('.block-result__watermark').css('left', 0);
+		$('.block-result__watermark').css('top', 0);
+		$(".block-transparency__slider").slider( "option", "value", 0.5 );
 	}
 };
